97706599884ba8cba1c7d80b60095ddf28693726
giospes <giovannijacopo.spes@gmail.com>
2023-03-15 00:13:34 +0100

    carousel scrolling done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,36 @@ const img = [
 ];
 const slide = document.querySelector(".gs-slide");
 const carousel = document.querySelector(".gs-carousel");
+let currentImg = 0, slides = '';
+
+
+
+for(let i=0; i<img.length; i++){
+    slides += `
+    <div class="gs-carousel-item">
+        <img src="./img/${img[i]}" alt="" class=" w-100 h-100 object-fit-cover gs-img-effect">
+    </div>
+    `
+}
+
+carousel.innerHTML += slides;
+document.querySelectorAll('.gs-img-effect')[currentImg].classList.add('active');
+
+
+const btnN = document.querySelector(".next")
+const btnP = document.querySelector(".previous")
+let show = document.querySelector('.active').outerHTML
+slide.innerHTML = `${show}`
+
+btnN.addEventListener('click', goNext)
+
+
+
+function goNext(){
+    document.querySelectorAll('.gs-img-effect')[currentImg].classList.remove('active');
+    currentImg++;
+    document.querySelectorAll('.gs-img-effect')[currentImg].classList.add('active');
+}
+
+
+
