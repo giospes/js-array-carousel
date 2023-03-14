dafa14fb1e7c6bc36e43293f483c05461723111d
giospes <giovannijacopo.spes@gmail.com>
2023-03-14 16:52:06 +0100

    buttons added

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const img = [
+    "01.webp","02.webp","03.webp","04.webp","05.webp"
+];
+const slide = document.querySelector(".gs-slide");
+const carousel = document.querySelector(".gs-carousel");
